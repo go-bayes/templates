f0ad9657366d4fd288e3c46c4429c07144e02fd7
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2022-08-25 14:50:11 +1200

    functions added

---
+++ functions/funs.R
@@ -0,0 +1,549 @@
+# functions.R
+library("here")
+library("fs")
+library("stdReg")
+library("ggplot2")
+library("mice")
+#library("gghighlight")
+library("conflicted")
+#conflict_prefer("pool", "mice")
+#conflict_prefer("filter", "dplyr")
+#conflict_prefer("select", "dplyr")
+#conflict_prefer("cbind", "base")
+
+# set paths for pushing files (off of github)  ## for jb only
+push_mods <- fs::path_expand("~/The\ Virtues\ Project\ Dropbox/outcomewide/mods")
+push_figs <- fs::path_expand("~/Users/joseph/The\ Virtues\ Project\ Dropbox/outcomewide/figs")
+pull_path <- fs::path_expand("~/The\ Virtues\ Project\ Dropbox/Joseph\ Bulbulia/00Bulbulia\ Pubs/2021/DATA/ldf.5")
+
+
+# read data
+
+read_my_data <- function(){
+   dat <- readRDS(pull_path)
+  return(dat)
+}
+
+## function for saving
+saveh <- function(df, name) {
+  x = df
+  saveRDS( x,  here::here(push_mods,  paste0(name, '')))
+}
+
+
+# function for reading
+readh <- function(name) {
+  df = readRDS(here::here(push_mods,  paste0(name, '')))
+  df
+}
+
+
+
+# function for mice models
+pool_stglm <- function(models, df, m, x, X) {
+  nx <- length(x)
+  est.all <- matrix(nrow = nx, ncol = m)
+  var.all <- matrix(nrow = nx, ncol = m)
+  for (i in 1:m) {
+    g.comp <-
+      stdGlm(
+        fit = models$analyses[[i]],
+        data = complete(df, i),
+        X = X,
+        x = x
+      )
+    est.all[, i] <- g.comp$est
+    var.all[, i] <- diag(g.comp$vcov)
+  }
+  #estimate
+  est <- rowMeans(est.all)
+
+  #within-variance
+  W <- rowMeans(var.all)
+
+  #between-variance
+  B <- apply(X = est.all, MARGIN = 1, FUN = var)
+
+  #total variance
+  var <- W + (1 + 1 / m) * B
+
+  #total standard error
+  se <- sqrt(var)
+  #confidence intervals
+  ci <- cbind(est - 1.96 * se, est + 1.96 * se)
+  # lower interval
+  ui <- est + (1.96 * se)
+  #upper interval
+  li <- est - (1.96 * se)
+  # row units
+  row <- x
+  # make data frame
+  outp <- as.data.frame(cbind(row, est, se, ui, li))
+  outp
+}
+
+
+# base R plot for mice pool_stglm outputs  (above)
+plot_stglm <- function(out, ylim, main, xlab, ylab) {
+  plot(
+    out$row,
+    out$est,
+    type = "l",
+    ylim = ylim,
+    main = main,
+    xlab = xlab,
+    ylab = ylab,
+    col.main = "black",
+    #sub="My Sub-title", col.sub="black",
+    col.lab = "black",
+    cex.lab = 0.75
+  )
+  polygon(c(x, rev(x)),
+          c(out$li, rev(out$ui)),
+          col = "grey75",
+          border = FALSE)
+  lines(out$row, out$est, lwd = 1)
+  lines(out$row, out$li, col = "red", lty = 2)
+  lines(out$row, out$ui, col = "red", lty = 2)
+}
+
+
+# make red dot
+# plot(x, x, col=ifelse(x==3, "red", "black"),
+#      pch=ifelse(x==3, 19, 1), cex=ifelse(x==3, 2, 1))
+
+
+#
+# # point
+# plot_stglm <- function(out, ylim, main, xlab, ylab) {
+#   plot(
+#     out$row,
+#     out$est,
+#     type = "l",
+#     ylim = ylim,
+#     main = main,
+#     xlab = xlab,
+#     ylab = ylab,
+#     col.main = "black",
+#     #sub="My Sub-title", col.sub="black",
+#     col.lab = "black",
+#     cex.lab = 0.75
+#   )
+#   polygon(c(x, rev(x)),
+#           c(out$li, rev(out$ui)),
+#           col = "grey75",
+#           border = FALSE)
+#   lines(out$row, out$est, lwd = 1)
+#   lines(out$row, out$li, col = "red", lty = 2)
+#   lines(out$row, out$ui, col = "red", lty = 2)
+# }
+# function for ggplot
+
+## function for contrasts using multiply imputed data WOW
+
+pool_stglm_contrast <- function(out, df, m, x, X, r) {
+  nx <- length(x)
+  est.all <- matrix(nrow=nx, ncol=m)
+  var.all <- matrix(nrow=nx, ncol=m)
+  for(i in 1:m){
+    g.comp <- stdGlm(fit=out$analyses[[i]], data=complete(df, i), X= X, x=x)
+    ss <- summary(object=g.comp, contrast="difference", reference=r)
+    #est.all[, i] <- g.comp$est
+    est.all[, i] <- ss$est.table[, "Estimate"]
+    #var.all[, i] <- diag(g.comp$vcov)
+    var.all[, i] <- ss$est.table[, "Std. Error"]^2
+  }
+
+  #estimate
+  est <- rowMeans(est.all)
+
+  #within-variance
+  W <- rowMeans(var.all)
+
+  #between-variance
+  B <- apply(X=est.all, MARGIN=1, FUN=var)
+
+  #total variance
+  var <- W+(1+1/m)*B
+
+  #total standard error
+  se <- sqrt(var)
+
+  #confidence intervals
+  ci <- cbind(est-1.96*se, est+1.96*se)
+
+  # lower interval
+  ui <- est + (1.96 * se)
+
+  #upper interval
+  li <- est - (1.96 * se)
+  # row units
+  row <- x
+  # make data frame
+  outp <- as.data.frame(cbind(row, est, se, ui, li))
+  outp
+}
+
+
+
+pool_stglm_contrast_ratio <- function(out, df, m, x, X, r) {
+  nx <- length(x)
+  est.all <- matrix(nrow=nx, ncol=m)
+  var.all <- matrix(nrow=nx, ncol=m)
+  for(i in 1:m){
+    g.comp <- stdGlm(fit=out$analyses[[i]], data=complete(df, i), X= X, x=x)
+    ss <- summary(object=g.comp, contrast="ratio", reference=r)
+    #est.all[, i] <- g.comp$est
+    est.all[, i] <- ss$est.table[, "Estimate"]
+    #var.all[, i] <- diag(g.comp$vcov)
+    var.all[, i] <- ss$est.table[, "Std. Error"]^2
+  }
+
+  #estimate
+  est <- rowMeans(est.all)
+
+  #within-variance
+  W <- rowMeans(var.all)
+
+  #between-variance
+  B <- apply(X=est.all, MARGIN=1, FUN=var)
+
+  #total variance
+  var <- W+(1+1/m)*B
+
+  #total standard error
+  se <- sqrt(var)
+
+  #confidence intervals
+  ci <- cbind(est-1.96*se, est+1.96*se)
+
+  # lower interval
+  ui <- est + (1.96 * se)
+
+  #upper interval
+  li <- est - (1.96 * se)
+  # row units
+  row <- x
+  # make data frame
+  outp <- as.data.frame(cbind(row, est, se, ui, li))
+  outp
+}
+
+
+
+plot_stglm <- function(out, ylim, main, xlab, ylab) {
+  plot(
+    out$row,
+    out$est,
+    type = "l",
+    ylim = ylim,
+    main = main,
+    xlab = xlab,
+    ylab = ylab,
+    col.main = "black",
+    sub="Marginal predictions by g-computation",
+    col.sub="black",
+    col.lab = "black",
+    cex.lab = 0.75
+  )
+  polygon(c(x, rev(x)),
+          c(out$li, rev(out$ui)),
+          col = "grey75",
+          border = FALSE)
+  lines(out$row, out$est, lwd = 1)
+  lines(out$row, out$li, col = "red", lty = 2)
+  lines(out$row, out$ui, col = "red", lty = 2)
+}
+
+
+## Contrast plot
+plot_stglm_contrast <- function(out, ylim, main, xlab, ylab) {
+  plot(
+    out$row,
+    out$est,
+    type = "l",
+    ylim = ylim,
+    main = main,
+    xlab = xlab,
+    ylab = ylab,
+    col.main = "black",
+    sub="Marginal contrasts relative to baseline by g-computation",
+    col.sub="black",
+    col.lab = "black",
+    cex.lab = 0.75
+  )
+  polygon(c(x, rev(x)),
+          c(out$li, rev(out$ui)),
+          col = "grey75",
+          border = FALSE)
+  lines(out$row, out$est, lwd = 1)
+  lines(out$row, out$li, col = "red", lty = 2)
+  lines(out$row, out$ui, col = "red", lty = 2)
+}
+
+# function for ggplot g-comp
+ggplot_stglm <- function(out, ylim, main, xlab, ylab, min, p, r) {
+  require(ggplot2)
+  g1 <- out[match(p, x),]
+  g1
+  g2 <- out[match(r, x),]
+  g2
+  ggplot2::ggplot(out, aes(x = row, y = est)) + 
+    geom_point() + 
+    geom_pointrange(aes(ymin =  li, ymax = ui), colour = "darkgray")  +
+    scale_y_continuous(limits = ylim) +
+    labs(
+      title = main,
+     # subtitle = "Marginal predictions by g-computation",
+     subtitle = "Marginal contrasts relative to baseline by g-computation",
+      x = xlab,
+      y = ylab
+    ) +
+    geom_pointrange(data=g1, aes(ymin = li, ymax = ui), colour="red") +  # highlight contrast
+   # geom_pointrange(data=g2, aes(ymin = li, ymax = ui), colour="black") +  # highlight contrast
+    theme_classic()
+}
+
+
+# function for ggplot contrast
+
+ggplot_stglm_contrast <- function(out, ylim, main, xlab, ylab, s) {
+  require(ggplot2)
+  g1 <- out[match(s, x),]
+  g1
+  ggplot2::ggplot(out, aes(x = row, y = est)) +
+    geom_point(colour = "black") + 
+    geom_pointrange(aes(ymin =  li, ymax = ui))  +
+    scale_y_continuous(limits = ylim) +
+    labs(
+      title = main,
+      subtitle = "Marginal contrasts relative to baseline by g-computation",
+      x = xlab,
+      y = ylab
+    ) + geom_pointrange(data=g1, aes(ymin = li, ymax = ui), colour="red") +  # highlight contrast
+    # this adds a red point geom_text(data=g1, label="Contrast", vjust=1) + # this adds a label for the red point
+    theme_classic()
+}
+
+
+ggplot_stglm_contrast2 <- function(out, ylim, main, xlab, ylab, r) {
+  require(ggplot2)
+  out <- out[2,]
+  ggplot2::ggplot(out, aes(x = row, y = est)) +
+    geom_point(colour = "black") +
+    geom_pointrange(aes(ymin =  li, ymax = ui))  +
+    scale_y_continuous(limits = ylim) +
+    geom_hline(yintercept=r, colour = "red") + 
+    labs(
+      title = main,
+      subtitle = "Marginal contrasts relative to baseline by g-computation",
+      x = xlab,
+      y = ylab
+    ) +
+    theme_classic()
+}
+
+
+
+## FUNCTION FOR OUTCOME WIDE REGRESSION
+
+# Y is the outcome
+# df is the dataframe -- a mice object
+# X is the exposure,
+# we assume a spline model
+
+mice_gaussian = function(df, X, Y) {
+    require("splines")
+    require("mice")
+  out <- with(df, glm(as.formula(
+    paste(
+      paste(Y, "~ bs(", X , ")+"),
+      paste(baselinevars,
+            collapse = "+")
+    )
+  )))
+  out
+}
+
+
+mice_generalised = function(df, X, Y, family) {
+    require("splines")
+    require("mice")
+  out <- with(df, glm(as.formula(
+    paste(
+      paste(Y, "~ bs(", X , ")+"),
+      paste(baselinevars,
+            collapse = "+")
+    )
+  ), family = family ))
+  out
+}
+
+
+
+
+
+# library(mice)
+# library(stdReg)
+# data(nhanes2)
+#
+# m <- 5
+# imp_dat <- mice(data=nhanes2, printFlag=FALSE, seed=0, m=m)
+# models <- with(imp_dat, glm(bmi~chl+hyp))
+#
+# x <- 180:190
+# nx <- length(x)
+# est.all <- matrix(nrow=nx, ncol=m)
+# var.all <- matrix(nrow=nx, ncol=m)
+# for(i in 1:m){
+#   g.comp <- stdGlm(fit=models$analyses[[i]], data=complete(imp_dat, i), X="chl", x=x)
+#   ss <- summary(object=g.comp, contrast="difference", reference=180)
+#   #est.all[, i] <- g.comp$est
+#   est.all[, i] <- ss$est.table[, "Estimate"]
+#   #var.all[, i] <- diag(g.comp$vcov)
+#   var.all[, i] <- ss$est.table[, "Std. Error"]^2
+# }
+#
+# #estimate
+# est <- rowMeans(est.all)
+#
+# #within-variance
+# W <- rowMeans(var.all)
+#
+# #between-variance
+# B <- apply(X=est.all, MARGIN=1, FUN=var)
+#
+# #total variance
+# var <- W+(1+1/m)*B
+#
+# #total standard error
+# se <- sqrt(var)
+#
+# #confidence intervals
+# ci <- cbind(est-1.96*se, est+1.96*se)
+
+
+# alternatives tbc
+# require(gplots)
+# gplots::plotCI(out$row, out$est, ui=out$ui, li=out$li)
+# plotrix::plotCI(out$row, out$est, ui=out$ui, li=out$li)
+
+
+# test data
+# imp_dat <- mice(data=nhanes2, printFlag=FALSE, seed=0, m=m)
+# models <- with(imp_dat, glm(bmi~chl+hyp))
+
+
+# useful code snippet
+# m18_SELF.ESTEEM <- lapply(1:10, function(i) {
+#   m <- glm(SELF.ESTEEM_st, data = complete(inc_m, action = i))
+# })
+
+
+
+
+#
+# library(MatchThem)
+# library(optmatch)
+# models_hw <- weightthem(Hours.Work_lead1_z ~
+#                           Hours.Work_z +
+#                           AGREEABLENESS_z +
+#                           CONSCIENTIOUSNESS_z +
+#                           EXTRAVERSION_z  +
+#                           HONESTY_HUMILITY_z +
+#                           NEUROTICISM_z +
+#                           OPENNESS_z +
+#                           Age_z +
+#                           Alcohol.Frequency_z + #
+#                           Alcohol.Intensity_log_z + #
+#                           Bodysat_z +
+#                           Believe.God_z +
+#                           Believe.Spirit_z +
+#                           BELONG_z + #
+#                           CharityDonate_log_z + #
+#                           ChildrenNum_z +
+#                           Church_z +
+#                           community +
+#                           Edu_z +
+#                           Employed_z +
+#                           EmotionRegulation1_z +
+#                           EmotionRegulation2_z +
+#                           EmotionRegulation3_z +
+#                           Euro_z +
+#                           GRATITUDE_z +
+#                           HomeOwner_z +
+#                           Hours.Exercise_log_z +
+#                           # Hours.Work_z +
+#                           HLTH.BMI_z  + #
+#                           HLTH.Fatigue_z + #
+#                           income_log_z +
+#                           ImpermeabilityGroup_z +
+#                           KESSLER6sum_z + #
+#                           LIFEMEANING_z + #
+#                           LIFESAT_z + #
+#                           Male_z +
+#                           NZdep_z +
+#                           NWI_z +
+#                           NZSEI13_z +
+#                           Parent_z +
+#                           Partner_z +
+#                           PERFECTIONISM_z +
+#                           PermeabilityIndividual_z +
+#                           Pol.Orient_z +
+#                           POWERDEPENDENCE1_z + #
+#                           POWERDEPENDENCE2_z + #
+#                           # PWI_z +
+#                           Relid_z +
+#                           Respect.Self_z + #
+#                           Rumination_z + #
+#                           SELF.CONTROL_z + #
+#                           SELF.ESTEEM_z + #
+#                           SexualSatisfaction_z +#
+#                           SFHEALTH_z +#
+#                           Smoker_z +#
+#                           Spiritual.Identification_z +
+#                           Standard.Living_z +
+#                           SUPPORT_z +#
+#                           Urban_z +
+#                           VENGEFUL.RUMIN_z +
+#                           Volunteers_z +
+#                           Your.Health_z +
+#                           Your.Future.Security_z +
+#                           Your.Personal.Relationships_z,
+#                         out2_sl,
+#                         approach = "within",
+#                         estimand = "ATE",
+#                         stabilize = TRUE,
+#                         method = "ebal")
+#
+#
+# saveh(models_hw,"models_hw")
+#
+#
+# sum<- summary(models_hw)
+# plot(sum)
+# sum
+# bal.tab(models_hw)
+#
+#
+# ctrim_st <- trim(models_hw, at = .998)
+# bal.tab(ctrim_st)
+# summary(ctrim_st)
+#
+#
+# # iptw models  STANDARD LIVING -------------------------------------------------------------
+# # no need to trim
+#
+#
+# out <- with(ctrim_st, glm( HLTH.BMI_lead2_z ~ Hours.Work_lead1_z ))
+# output <- pool(out, dfcom = NULL)
+# summary(output, conf.int = TRUE)
+#
+# round( EValue::evalues.OLS( , se = , sd = 1, delta = 2, true = 0), 3)
+# round( EValue::evalues.RR(, lo =  , hi =, true = 1), 3)
+
+# test <- df_in %>%
+#   mutate(inc_diff = (Household.INC_100 - Household.INC_100_lead1) * 100000) %>%
+#   select(Id, inc_diff) %>%
+#   arrange(inc_diff)
+# test %>% as_tibble() %>% print(n=500)
---
+++ functions/libs.R
@@ -0,0 +1,44 @@
+#libs.R
+
+#libraries ised
+library("dplyr") # data wrangling
+library("tidyr") # data wrangling
+library("janitor") # clean var names
+library("here") # directory management
+library("skimr") # data inspection
+#library("Amelia") # multiple imputation
+library("ggplot2") # graphics
+library("purrr") # data analysis
+library("patchwork") # graphics
+library("kableExtra") #tables
+library("parameters") # working with models
+library("mice") # multiple imputation
+library("table1") # nice tables (html, latex)
+library("ggokabeito")   # color palette
+#library("brms") # bayesian estimation
+#library("rstan")
+library("ggpubr") # themes and also graphics manipulation
+#library("MatchThem") # weights for propensity scores with multiple datasets
+library("marginaleffects") # what it says
+#library("cobalt") # Weights for propensity scores
+#library("WeightIt")# Weights for propensity scores
+#library("optmatch")# Weights for propensity scores
+#library("cmdstanr") # Bayesian estimation
+library("formula.tools") #
+library("CMAverse") # causal mediation
+library("splines") # estimation
+library("gghighlight") # highlight points
+library("parameters") # working with models
+library("naniar") # missing data
+library("conflicted") # don't have conflicts #
+library("Amelia") # multiple imputation
+library("miceadds") # convert amelia to mice objects and vice versa
+#conflict_prefer("pool", "mice")
+#conflict_prefer("filter", "dplyr")
+#conflict_prefer("select", "dplyr")
+#conflict_prefer("cbind", "base")
+#library("speedglm") # not needed currently
+library("stdReg") # g-computation !
+#rstan_options(auto_write = TRUE) # bayesian estimation
+options(mc.cores = parallel::detectCores ()) # use all course
+theme_set(theme_pubclean()) # nice theme
